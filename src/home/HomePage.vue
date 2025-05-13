0023ddbca793f1ef9e960fa5c456e87137120d15
Юрченко Александр <YurchenkoAS@empl.vvsu.ru>
2025-05-13 14:12:18 +1000

    1st deploy

---
+++ src/home/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <div>
+      <img class="robot" src="../assets/robot-home.png" aria-hidden="true">
+    </div>
+    <div>
+      <router-link to="/build">Начни</router-link> собирать робота
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+  .home{
+    text-align: center;
+  }
+  .robot{
+    height: 300px;
+  }
+</style>
